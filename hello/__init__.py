df2c22a50b2c95643ade3627c83399ccf73d8b86
JC-09 <jonathancen0906@gmail.com>
2020-09-17 13:46:20 +1200

    revision

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,5 +1,8 @@
+#__init__.py
 from flask import Flask, render_template
 
+
+# wsgi.py will execute the following functions
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +12,7 @@ def create_app():
         return 'Homepage'
 
     @app.route('/greet/<name>')
-    def say_hello(name: str):
+    def say_hello(name: str): # The value of name is provided in the url, e.g. http://localhost:5000/greet/Jonathan will produce "Hello, Jonathan"
         return render_template('hello.html', name=name)
 
     @app.route('/people')
@@ -19,7 +22,7 @@ def create_app():
         return render_template('people.html', people=list_of_people)
 
     @app.route('/<name>')
-    def make_html_page(name: str):
+    def make_html_page(name: str):  # This method renders any extra html pages that were not yet rendered above.
         return render_template(name + '.html', message='Hello!')
 
     return app
